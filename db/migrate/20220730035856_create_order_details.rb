0f5736d81eaf8b160eb8ab79c34e5ecb4092ba1c
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-07-30 05:05:29 +0000

    [Add]nagano_cake_テーブル作成

---
+++ db/migrate/20220730035856_create_order_details.rb
@@ -0,0 +1,13 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.references :item,     foreign_key: true
+      t.references :order,    foreign_key: true
+      t.integer :price,              null: false
+      t.integer :amount,             null: false
+      t.integer :making_status,      null: false
+
+      t.timestamps
+    end
+  end
+end
