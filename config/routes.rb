072fa4c23465516833737a02ce5ef1fe5ebbf3f9
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-07-30 05:38:35 +0000

    [Add]nagano_cake_コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,25 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # 会員側のルーティング設定
-  get 'items' => 'public/items#index'
+  namespace :public do
+    resources :homes
+    resources :items
+    resources :registrations
+    resources :sessions
+    resources :customers
+    resources :cart_items
+    resources :orders
+    resources :adresses
+  end
   # 管理者側のルーティング設定
   namespace :admin do
-    get 'items' => 'admin/items#index'
+    resources :items
+    resources :genres
+    resources :customers
+    resources :homes
+    resources :orders
+    resources :order_details
+    resources :sessions
   end
 
 
