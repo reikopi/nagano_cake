6d7344511f18c876f748f40494b1e9f1bfa5041d
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-07-30 05:12:17 +0000

    [Add]nagano_cake_namespaceを設定するためのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # 会員側のルーティング設定
+  get 'items' => 'public/items#index'
+  # 管理者側のルーティング設定
+  namespace :admin do
+    get 'items' => 'admin/items#index'
+  end
+
+
 end
