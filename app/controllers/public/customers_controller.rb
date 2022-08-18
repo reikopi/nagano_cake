690c04f2eef3533f1facefd1d2890b177a684880
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-08-18 13:54:27 +0000

    [Add]nagano_cake_配送先一覧編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,9 +5,13 @@ def show
 end
 
 def edit
+  @customer = Customer.find(params[:id])
 end
 
 def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to public_customer_path
 end
 
 def check
