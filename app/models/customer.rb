04a604de0b5c373df8e9b8a2f998f481453f8d7c
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-08-28 03:32:20 +0000

    [Add]nagano_cake_新規ユーザー登録画面修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
-  
-  
-  
+
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :encrypted_password, :password_confirmation, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
