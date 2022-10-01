c704da4b1f0ae8a43aa027c9e6533a98600db7be
reikopi <rk02.stmi.1018@gmail.com>
2022-10-01 01:46:34 +0000

    [修正]nagano_cake_public/redistration/会員_新規登録後の遷移先

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -7,7 +7,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-   public_items_path(current_customer)
+   public_customer_path
   end
 
   def after_sign_out_path_for(resource)
