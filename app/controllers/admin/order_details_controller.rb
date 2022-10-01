33a7d2de3e34528d40c63ecc6ca34cffe3d188cf
reikopi <rk02.stmi.1018@gmail.com>
2022-10-01 05:24:38 +0000

    [修正]nagano_cake_public_エラー修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrderDetailsController < ApplicationController
 def update
   @order_detail = OrderDetail.find(params[:id])
   @order_detail.update(making_status: params[:order_detail][:making_status])
-  redirect_to admin_order_path(@order_detail.id)
+  redirect_to admin_order_path(@order_detail.order.id)
 end
 
 
